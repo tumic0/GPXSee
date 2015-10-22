a08a3d2bb459f686d0b4b41e96ac3bb4ac195208
Martin Tůma <tumic@cbox.cz>
2015-10-22 09:58:07 +0200

    Version 1.0

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 
 #define APP_NAME       "GPXSee"
 #define APP_HOMEPAGE   "http://tumic.wz.cz/gpxsee"
-#define APP_VERSION    "0.1"
+#define APP_VERSION    "1.0"
 
 #define FONT_FAMILY    "Arial"
 #define FONT_SIZE      12
