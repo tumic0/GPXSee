53147b5e6e0cfff748d49d4532b4c46f576fff88
Martin Tůma <tumic@cbox.cz>
2017-12-01 22:24:04 +0100

    Code cleanup

--- src/data/path.cpp
+++ src/data/path.cpp
@@ -2,11 +2,12 @@
 
 RectC Path::boundingRect() const
 {
-	if (size() < 2)
-		return RectC();
+	RectC ret;
 
-	RectC ret(first().coordinates(), first().coordinates());
-	for (int i = 1; i < size(); i++)
+	if (size() < 2)
+		return ret;
+
+	for (int i = 0; i < size(); i++)
 		ret.unite(at(i).coordinates());
 
 	return ret;
