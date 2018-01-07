a22b3b2cb45fb3ebbac1f3688642f59100384d53
ninelima <ninelima@yahoo.com>
2018-01-07 10:53:18 +0800

    Incorporate negative altitude mod for igc Clean units defs

--- src/GUI/units.h
+++ src/GUI/units.h
@@ -12,7 +12,7 @@ enum Units {
 #define M2FT    3.280839900000  // m -> ft
 #define MS2KMH  3.600000000000  // m/s -> km/h
 #define MS2MIH  2.236936290000  // m/s -> mi/h
-#define MS2KTS  1.943844        // m/s -> knots (kts)
+#define MS2KTS  1.943844000000  // m/s -> knots (kts)
 #define FT2MI   0.000189393939  // ft -> mi
 #define FT2NM   0.000164578800  // ft -> nm
 #define MM2IN   0.039370100000  // mm -> in
@@ -23,7 +23,7 @@ enum Units {
 #define KMINNM  0.539957 // 1 km in nm
 #define MIINFT  5280.0   // 1 mi in ft
 #define MIINM   1609.344 // 1 mi in m
-#define NMINM   1852     // 1 nm in m
+#define NMINM   1852.0   // 1 nm in m
 #define MININS  60.0     // 1 min in s
 #define HINS    3600.0   // 1 hins
 
