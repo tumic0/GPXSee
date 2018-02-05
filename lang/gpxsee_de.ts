fce0c49504229cf1b321c5a64bb9a89780277def
Martin Tůma <tumic@cbox.cz>
2018-02-05 20:39:44 +0100

    Added missing German strings

--- lang/gpxsee_de.ts
+++ lang/gpxsee_de.ts
@@ -302,32 +302,32 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="617"/>
         <source>Map directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kartenverzeichnis:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="618"/>
         <source>POI directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>POI-Verzeichnis:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="620"/>
         <source>GCS file:</source>
-        <translation type="unfinished"></translation>
+        <translation>GCS-Datei:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="621"/>
         <source>PCS file:</source>
-        <translation type="unfinished"></translation>
+        <translation>PCS-Datei:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="622"/>
         <source>Ellipsoids file:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ellipsoidendatei:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="625"/>
         <source>User override directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutzerverzeichnis:</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="634"/>
@@ -844,22 +844,22 @@
     <message>
         <location filename="../src/map/maplist.cpp" line="129"/>
         <source>OziExplorer maps</source>
-        <translation type="unfinished"></translation>
+        <translation>OziExplorer Karten</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="130"/>
         <source>TrekBuddy maps/atlases</source>
-        <translation type="unfinished"></translation>
+        <translation>TrekBuddy Karten/Atlanten</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="131"/>
         <source>GeoTIFF images</source>
-        <translation type="unfinished"></translation>
+        <translation>GeoTIFF Bilder</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="132"/>
         <source>Online map sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Online-Kartenquellen</translation>
     </message>
 </context>
 <context>
