e5db00a4f49193e7ba865caf9e491838de83fac7
Martin Tůma <tumic@cbox.cz>
2018-04-14 20:54:18 +0200

    Added missing density setting

--- icons/scripts/icns.sh
+++ icons/scripts/icns.sh
@@ -12,17 +12,17 @@ for e in $EXTENSIONS; do
 	ICONSET=$1.iconset
 	mkdir $ICONSET
 
-	convert -background none -resize '!16x16' "$1.svg" "$ICONSET/icon_16x16.png"
-	convert -background none -resize '!32x32' "$1.svg" "$ICONSET/icon_16x16@2x.png"
+	convert -density 400 -background none -resize '16x16' "$1.svg" "$ICONSET/icon_16x16.png"
+	convert -density 400 -background none -resize '32x32' "$1.svg" "$ICONSET/icon_16x16@2x.png"
 	cp "$ICONSET/icon_16x16@2x.png" "$ICONSET/icon_32x32.png"
-	convert -background none -resize '!64x64' "$1.svg" "$ICONSET/icon_32x32@2x.png"
-	convert -background none -resize '!128x128' "$1.svg" "$ICONSET/icon_128x128.png"
-	convert -background none -resize '!256x256' "$1.svg" "$ICONSET/icon_128x128@2x.png"
+	convert -density 400 -background none -resize '64x64' "$1.svg" "$ICONSET/icon_32x32@2x.png"
+	convert -density 400 -background none -resize '128x128' "$1.svg" "$ICONSET/icon_128x128.png"
+	convert -density 400 -background none -resize '256x256' "$1.svg" "$ICONSET/icon_128x128@2x.png"
 	cp "$ICONSET/icon_128x128@2x.png" "$ICONSET/icon_256x256.png"
-	convert -background none -resize '!512x512' "$1.svg" "$ICONSET/icon_256x256@2x.png"
+	convert -density 400 -background none -resize '512x512' "$1.svg" "$ICONSET/icon_256x256@2x.png"
 	cp "$ICONSET/icon_256x256@2x.png" "$ICONSET/icon_512x512.png"
-	convert -background none -resize '!1024x1024' "$1.svg" "$ICONSET/icon_512x512@2x.png"
+	convert -density 400 -background none -resize '1024x1024' "$1.svg" "$ICONSET/icon_512x512@2x.png"
 
 	iconutil -c icns -o $1.icns "$ICONSET"
-	rm -R "$ICONSET" $1.svg
+#	rm -R "$ICONSET" $1.svg
 done
--- icons/scripts/ico.sh
+++ icons/scripts/ico.sh
@@ -8,6 +8,6 @@ for e in $EXTENSIONS; do
 
 	EXT=`echo $1 | tr /a-z/ /A-Z/`
 	sed -e "s/\$EXTENSION/$EXT/" -e "s/\$COLOR/$2/" icon-template.svg > $1.svg
-	convert $1.svg -define icon:auto-resize $1.ico
+	convert -density 400 $1.svg -define icon:auto-resize $1.ico
 	rm $1.svg
 done
