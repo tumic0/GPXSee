4b776c8cc17f29583d417bdf41ff428c999a71b6
Martin Tůma <tumic@cbox.cz>
2018-04-15 10:42:06 +0200

    Replaced remaining qreals in Coordinates logic

--- src/common/rectc.cpp
+++ src/common/rectc.cpp
@@ -43,29 +43,29 @@ RectC RectC::operator|(const RectC &r) const
 	if (r.isNull())
 		return *this;
 
-	qreal l1 = _tl.lon();
-	qreal r1 = _tl.lon();
+	double l1 = _tl.lon();
+	double r1 = _tl.lon();
 	if (_br.lon() - _tl.lon() < 0)
 		l1 = _br.lon();
 	else
 		r1 = _br.lon();
 
-	qreal l2 = r._tl.lon();
-	qreal r2 = r._tl.lon();
+	double l2 = r._tl.lon();
+	double r2 = r._tl.lon();
 	if (r._br.lon() - r._tl.lon() < 0)
 		l2 = r._br.lon();
 	else
 		r2 = r._br.lon();
 
-	qreal t1 = _tl.lat();
-	qreal b1 = _tl.lat();
+	double t1 = _tl.lat();
+	double b1 = _tl.lat();
 	if (_br.lat() - _tl.lat() < 0)
 		t1 = _br.lat();
 	else
 		b1 = _br.lat();
 
-	qreal t2 = r._tl.lat();
-	qreal b2 = r._tl.lat();
+	double t2 = r._tl.lat();
+	double b2 = r._tl.lat();
 	if (r._br.lat() - r._tl.lat() < 0)
 		t2 = r._br.lat();
 	else
@@ -85,15 +85,15 @@ RectC RectC::operator&(const RectC &r) const
 	if (isNull() || r.isNull())
 		return RectC();
 
-	qreal l1 = _tl.lon();
-	qreal r1 = _tl.lon();
+	double l1 = _tl.lon();
+	double r1 = _tl.lon();
 	if (_br.lon() - _tl.lon() < 0)
 		l1 = _br.lon();
 	else
 		r1 = _br.lon();
 
-	qreal l2 = r._tl.lon();
-	qreal r2 = r._tl.lon();
+	double l2 = r._tl.lon();
+	double r2 = r._tl.lon();
 	if (r._br.lon() - r._tl.lon() < 0)
 		l2 = r._br.lon();
 	else
@@ -102,15 +102,15 @@ RectC RectC::operator&(const RectC &r) const
 	if (l1 > r2 || l2 > r1)
 		return RectC();
 
-	qreal t1 = _tl.lat();
-	qreal b1 = _tl.lat();
+	double t1 = _tl.lat();
+	double b1 = _tl.lat();
 	if (_br.lat() - _tl.lat() < 0)
 		t1 = _br.lat();
 	else
 		b1 = _br.lat();
 
-	qreal t2 = r._tl.lat();
-	qreal b2 = r._tl.lat();
+	double t2 = r._tl.lat();
+	double b2 = r._tl.lat();
 	if (r._br.lat() - r._tl.lat() < 0)
 		t2 = r._br.lat();
 	else
