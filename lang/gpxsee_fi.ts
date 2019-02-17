451c0c757f4ac99115e249c165c423f435b8a198
Nikolay Korotkiy <sikmir@gmail.com>
2019-02-17 08:04:32 +0000

    Translated using Weblate (Finnish)
    Currently translated at 98.5% (332 of 337 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/fi/

--- lang/gpxsee_fi.ts
+++ lang/gpxsee_fi.ts
@@ -449,7 +449,7 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="333"/>
         <source>Show areas</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytä alueet</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="338"/>
@@ -642,7 +642,7 @@
         <location filename="../src/GUI/gui.cpp" line="1022"/>
         <location filename="../src/GUI/gui.cpp" line="1090"/>
         <source>Areas</source>
-        <translation type="unfinished"></translation>
+        <translation>Alueet</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="1065"/>
@@ -1354,7 +1354,7 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="146"/>
         <source>Areas</source>
-        <translation type="unfinished"></translation>
+        <translation>Alueet</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="439"/>
@@ -1504,12 +1504,12 @@
     <message>
         <location filename="../src/GUI/areaitem.cpp" line="15"/>
         <source>Name</source>
-        <translation type="unfinished">Nimi</translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../src/GUI/areaitem.cpp" line="17"/>
         <source>Description</source>
-        <translation type="unfinished">Kuvaus</translation>
+        <translation>Kuvaus</translation>
     </message>
 </context>
 <context>
