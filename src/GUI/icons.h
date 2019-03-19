68644aa25d7b713988f9ad7882fec72e882c82c6
Martin Tůma <tumic@cbox.cz>
2019-03-20 00:00:08 +0100

    Icon files structure cleanup

--- src/GUI/icons.h
+++ src/GUI/icons.h
@@ -1,30 +1,30 @@
 #ifndef ICONS_H
 #define ICONS_H
 
-#define APP_ICON          ":/icons/gpxsee.png"
+#define APP_ICON          ":/gpxsee.png"
 
 // Toolbar/menu icons
-#define OPEN_FILE_ICON    ":/icons/document-open.png"
-#define EXPORT_FILE_ICON  ":/icons/document-export.png"
-#define PRINT_FILE_ICON   ":/icons/document-print.png"
-#define CLOSE_FILE_ICON   ":/icons/dialog-close.png"
-#define SHOW_POI_ICON     ":/icons/flag.png"
-#define SHOW_MAP_ICON     ":/icons/applications-internet.png"
-#define SHOW_GRAPHS_ICON  ":/icons/office-chart-line.png"
-#define QUIT_ICON         ":/icons/application-exit.png"
-#define RELOAD_FILE_ICON  ":/icons/view-refresh.png"
-#define NEXT_FILE_ICON    ":/icons/arrow-right.png"
-#define PREV_FILE_ICON    ":/icons/arrow-left.png"
-#define LAST_FILE_ICON    ":/icons/arrow-right-double.png"
-#define FIRST_FILE_ICON   ":/icons/arrow-left-double.png"
-#define FULLSCREEN_ICON   ":/icons/view-fullscreen.png"
+#define OPEN_FILE_ICON    ":/document-open.png"
+#define EXPORT_FILE_ICON  ":/document-export.png"
+#define PRINT_FILE_ICON   ":/document-print.png"
+#define CLOSE_FILE_ICON   ":/dialog-close.png"
+#define SHOW_POI_ICON     ":/flag.png"
+#define SHOW_MAP_ICON     ":/applications-internet.png"
+#define SHOW_GRAPHS_ICON  ":/office-chart-line.png"
+#define QUIT_ICON         ":/application-exit.png"
+#define RELOAD_FILE_ICON  ":/view-refresh.png"
+#define NEXT_FILE_ICON    ":/arrow-right.png"
+#define PREV_FILE_ICON    ":/arrow-left.png"
+#define LAST_FILE_ICON    ":/arrow-right-double.png"
+#define FIRST_FILE_ICON   ":/arrow-left-double.png"
+#define FULLSCREEN_ICON   ":/view-fullscreen.png"
 
 // Options dialog icons
-#define APPEARANCE_ICON   ":/icons/format-stroke-color.png"
-#define POI_ICON          ":/icons/flag_32.png"
-#define SYSTEM_ICON       ":/icons/preferences-system.png"
-#define PRINT_EXPORT_ICON ":/icons/document-print_32.png"
-#define DATA_ICON         ":/icons/view-filter.png"
-#define MAPS_ICON         ":/icons/applications-internet_32.png"
+#define APPEARANCE_ICON   ":/format-stroke-color.png"
+#define POI_ICON          ":/flag_32.png"
+#define SYSTEM_ICON       ":/preferences-system.png"
+#define PRINT_EXPORT_ICON ":/document-print_32.png"
+#define DATA_ICON         ":/view-filter.png"
+#define MAPS_ICON         ":/applications-internet_32.png"
 
 #endif /* ICONS_H */
