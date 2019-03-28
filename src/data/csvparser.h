061b8d6a63563dc827b3fe2e0be645c030d29aa8
Todor Trundev <ttrundev@visteon.com>
2019-03-28 15:40:53 +0200

    Wheellog CSV support: prototype version
    Modify CSVParser to support logs from Android WheelLog app:
    https://play.google.com/store/apps/details?id=com.cooper.wheellog
    The log must include location entries, i.e. the "Settings -> Log
    Settings -> Log location" must be enabled.
    
    If the original Garmin waypoint parser fails, the newly added
    parse_wheellog() methid is called. All the current EUC parameters are
    extracted from the log, but just the speed, power, temperature, mode and
    alert are displayed.
    
    The mode and alert generate waypoints. The speed reported by the EUC is
    not displayed correctly, as the GPXSee overrides it with some auto-
    generated values.

--- src/data/csvparser.h
+++ src/data/csvparser.h
@@ -16,6 +16,11 @@ public:
 private:
 	QString _errorString;
 	int _errorLine;
+
+	bool parse_waypoints(QFile *file, QList<TrackData> &tracks, QList<RouteData> &routes,
+		QList<Area> &polygons, QVector<Waypoint> &waypoints);
+	bool parse_wheellog(QFile *file, QList<TrackData> &tracks, QList<RouteData> &routes,
+		QList<Area> &polygons, QVector<Waypoint> &waypoints);
 };
 
 #endif // CSVPARSER_H
