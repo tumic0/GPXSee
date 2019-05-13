3c6ec704abc54c2d0f1507b4123e04cbeb496f6c
Mário Machado <mario.spm@mail.com>
2019-05-13 05:55:02 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (341 of 341 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/pt_BR/

--- lang/gpxsee_pt_BR.ts
+++ lang/gpxsee_pt_BR.ts
@@ -1007,7 +1007,7 @@
     <message>
         <location filename="../src/map/maplist.cpp" line="123"/>
         <source>Garmin IMG maps</source>
-        <translation type="unfinished"></translation>
+        <translation>Mapas Garmin IMG</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="125"/>
