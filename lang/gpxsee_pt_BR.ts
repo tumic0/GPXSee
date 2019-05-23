019bd4de109319bd0cef83ddf43615cdf7cbfb89
Mário Machado <mario.spm@mail.com>
2019-05-23 19:50:24 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (342 of 342 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/pt_BR/

--- lang/gpxsee_pt_BR.ts
+++ lang/gpxsee_pt_BR.ts
@@ -470,7 +470,7 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="352"/>
         <source>km/mi markers</source>
-        <translation type="unfinished"></translation>
+        <translation>Marcadores de km/mi</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="359"/>
