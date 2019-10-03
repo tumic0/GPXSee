04ddcecb70103c8ee390524e0db5815554b10d9e
Martin Tůma <tumic@cbox.cz>
2019-10-03 22:47:31 +0200

    Increased tile cache size

--- src/GUI/settings.h
+++ src/GUI/settings.h
@@ -152,7 +152,7 @@
 #define ENABLE_HTTP2_SETTING              "enableHTTP2"
 #define ENABLE_HTTP2_DEFAULT              true
 #define PIXMAP_CACHE_SETTING              "pixmapCache"
-#define PIXMAP_CACHE_DEFAULT              64 /* MB */
+#define PIXMAP_CACHE_DEFAULT              256 /* MB */
 #define CONNECTION_TIMEOUT_SETTING        "connectionTimeout"
 #define CONNECTION_TIMEOUT_DEFAULT        30 /* s */
 #define HIRES_PRINT_SETTING               "hiresPrint"
