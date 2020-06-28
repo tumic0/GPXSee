c1b79217a9f8015b05e5751cbe91b4a3380ecbc4
Nikolay Korotkiy <sikmir@gmail.com>
2020-06-28 22:02:05 +0300

    Update gpxsee_en.ts (#296)

--- lang/gpxsee_en.ts
+++ lang/gpxsee_en.ts
@@ -1,4 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
+<context>
+    <name>GUI</name>
+    <message numerus="yes">
+        <location filename="../src/GUI/gui.cpp" line="1392"/>
+        <source>%n files</source>
+        <translation>
+            <numerusform>%n file</numerusform>
+            <numerusform>%n files</numerusform>
+        </translation>
+    </message>
+</context>
 </TS>
