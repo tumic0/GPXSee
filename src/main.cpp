3916a922063322596ccd5769e3214be5f2e38dc2
Martin Tůma <tumic@cbox.cz>
2020-12-22 01:17:38 +0100

    Cleanup forgotten stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-//#include "GUI/timezoneinfo.h"
 #include "GUI/app.h"
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-//	qRegisterMetaTypeStreamOperators<TimeZoneInfo>("TimeZoneInfo");
+#endif // QT6
 
 	App app(argc, argv);
 	return app.run();
