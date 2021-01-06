cec8946d864ccf1cb71557a033a8bb4932ef2619
Martin Tůma <tumic@cbox.cz>
2021-01-06 19:27:28 +0100

    Fixed windows icons background

--- icons/formats/scripts/ico.sh
+++ icons/formats/scripts/ico.sh
@@ -5,6 +5,6 @@ while read e; do
 
 	EXT=`echo $1 | tr /a-z/ /A-Z/`
 	sed -e "s/\$EXTENSION/$EXT/" -e "s/\$COLOR/$2/" icon-template.svg > $1.svg
-	convert -density 400 $1.svg -define icon:auto-resize $1.ico
+	convert -density 400 -background none $1.svg -define icon:auto-resize $1.ico
 	rm $1.svg
 done < extensions
