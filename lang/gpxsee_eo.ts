f99378e3deba20eb650c491903d46ac0c4e93de3
Nikolay Korotkiy <sikmir@gmail.com>
2021-02-09 07:48:41 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 95.1% (371 of 390 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/eo/

--- lang/gpxsee_eo.ts
+++ lang/gpxsee_eo.ts
@@ -988,7 +988,7 @@
     <message>
         <location filename="../src/map/maplist.cpp" line="101"/>
         <source>AlpineQuest maps</source>
-        <translation type="unfinished"></translation>
+        <translation>AlpineQuest-mapoj</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="102"/>
