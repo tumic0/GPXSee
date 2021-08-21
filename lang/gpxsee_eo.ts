a4c74497721d312c183b1c5772170cc359f6fcb1
Nikolay Korotkiy <sikmir@gmail.com>
2021-08-21 15:33:46 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 95.5% (389 of 407 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/eo/

--- lang/gpxsee_eo.ts
+++ lang/gpxsee_eo.ts
@@ -114,7 +114,7 @@
     <message>
         <location filename="../src/data/data.cpp" line="152"/>
         <source>ONmove files</source>
-        <translation type="unfinished"></translation>
+        <translation>ONmove-dosieroj</translation>
     </message>
     <message>
         <location filename="../src/data/data.cpp" line="153"/>
