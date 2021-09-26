2a7507ff2592d9e3bc6bf8bcb0dda9a20fe7a6b7
Martin Tůma <tumic@cbox.cz>
2021-09-26 11:33:50 +0200

    Fixed form layout issue on OS X

--- src/GUI/authenticationwidget.cpp
+++ src/GUI/authenticationwidget.cpp
@@ -5,6 +5,11 @@ AuthenticationWidget::AuthenticationWidget(QWidget *parent) : QWidget(parent)
 {
 	_username = new QLineEdit();
 	_password = new PasswordEdit();
+#ifdef Q_OS_MAC
+	/* A hack to fix the issue with different field sizes on Mac */
+	_username->setMinimumWidth(150);
+	_password->setMinimumWidth(150);
+#endif // Q_OS_MAC
 
 	QFormLayout *layout = new QFormLayout();
 	layout->addRow(tr("Username:"), _username);
