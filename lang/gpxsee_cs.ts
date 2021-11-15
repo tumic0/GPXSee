4a6293dd8655e4d875a5e86f5505c5235161fa7c
Martin Tůma <tumic@cbox.cz>
2021-11-15 20:33:04 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (423 of 423 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/cs/

--- lang/gpxsee_cs.ts
+++ lang/gpxsee_cs.ts
@@ -1129,7 +1129,7 @@
     <message>
         <location filename="../src/map/maplist.cpp" line="135"/>
         <source>QuickChart maps</source>
-        <translation type="unfinished"></translation>
+        <translation>QuickChart mapy</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="139"/>
