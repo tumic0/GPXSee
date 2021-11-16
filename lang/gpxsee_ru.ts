5298110a0ecee8144124bc6cab4837c1dc4fd4ba
Nikolay Korotkiy <sikmir@gmail.com>
2021-11-16 14:39:47 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (423 of 423 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/ru/

--- lang/gpxsee_ru.ts
+++ lang/gpxsee_ru.ts
@@ -1129,7 +1129,7 @@
     <message>
         <location filename="../src/map/maplist.cpp" line="135"/>
         <source>QuickChart maps</source>
-        <translation type="unfinished"></translation>
+        <translation>QuickChart карты</translation>
     </message>
     <message>
         <location filename="../src/map/maplist.cpp" line="139"/>
