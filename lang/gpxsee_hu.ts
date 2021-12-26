f24f2abb4d5b24c41cd8da764532b92f80b15d2e
99 efi <fegyed71@gmail.com>
2021-12-26 10:44:00 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (439 of 439 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/hu/

--- lang/gpxsee_hu.ts
+++ lang/gpxsee_hu.ts
@@ -157,7 +157,7 @@
     <message>
         <location filename="../src/data/data.cpp" line="167"/>
         <source>TwoNav files</source>
-        <translation type="unfinished"></translation>
+        <translation>TwoNav fájlok</translation>
     </message>
     <message>
         <location filename="../src/data/data.cpp" line="168"/>
