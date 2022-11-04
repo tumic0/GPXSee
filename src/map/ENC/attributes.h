8ef1aa0f4028c9c43444346d267d826410d1d993
Martin Tůma <tumic@cbox.cz>
2022-11-04 22:51:54 +0100

    Added missing attributes header file

---
+++ src/map/ENC/attributes.h
@@ -0,0 +1,12 @@
+#ifndef ENC_ATTRIBUTES_H
+#define ENC_ATTRIBUTES_H
+
+#define CATHAF 30
+#define CATLMK 35
+#define CATREA 56
+#define CATWRK 71
+#define DRVAL1 87
+#define OBJNAM 116
+#define VALDCO 174
+
+#endif // ENC_ATTRIBUTES_H
