c001235e9146da5c2142e347d89589a69fffeb75
Martin Tůma <tumic@cbox.cz>
2023-02-01 22:49:03 +0100

    Code cleanup

--- src/GUI/settings.cpp
+++ src/GUI/settings.cpp
@@ -6,11 +6,10 @@ static QString defaultPlugin()
 	QString source;
 
 	QGeoPositionInfoSource *ps = QGeoPositionInfoSource::createDefaultSource(0);
-	if (!ps)
-		return source;
-
-	source = ps->sourceName();
-	delete ps;
+	if (ps) {
+		source = ps->sourceName();
+		delete ps;
+	}
 
 	return source;
 }
