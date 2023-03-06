426490ef4d7c800048713e2368df6a2c52a5f732
mtriau <283571717@qq.com>
2023-03-06 00:11:31 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (467 of 467 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/zh_Hans/

--- lang/gpxsee_zh.ts
+++ lang/gpxsee_zh.ts
@@ -1911,7 +1911,7 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="749"/>
         <source>DEM cache size:</source>
-        <translation type="unfinished"></translation>
+        <translation>DEM 缓存大小：</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="769"/>
