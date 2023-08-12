a91f0d183f83fc554da2e1321d08e248e973d5da
ovl-1 <ovl-002@mailinator.com>
2023-08-12 10:47:45 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (468 of 468 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/nb_NO/

--- lang/gpxsee_nb.ts
+++ lang/gpxsee_nb.ts
@@ -728,7 +728,7 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="1022"/>
         <source>Don&apos;t show again</source>
-        <translation type="unfinished"></translation>
+        <translation>Ikke vis igjen</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="1852"/>
