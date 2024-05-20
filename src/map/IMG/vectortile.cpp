c4d07b5f126dc91673e7366424134826a5f51418
Martin Tůma <tumic@cbox.cz>
2024-05-20 21:19:26 +0200

    Prefer render quality rather than render speed

--- src/map/IMG/vectortile.cpp
+++ src/map/IMG/vectortile.cpp
@@ -276,7 +276,10 @@ void VectorTile::elevations(const RectC &rect, const Zoom &zoom,
 		}
 	}
 
-	int level = _dem->level(zoom);
+	// Shift the DEM level to get better data then what the map defines for
+	// the given zoom (we prefer rendering quality rather than speed). For
+	// maps with a single level this has no effect.
+	int level = _dem->level(zoom) / 2;
 	QList<const DEMTile*> tiles(_dem->tiles(rect, level));
 	for (int i = 0; i < tiles.size(); i++) {
 		const DEMTile *tile = tiles.at(i);
