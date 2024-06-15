d565d3b52607db3af978b7f5195af9a0afcc8cce
Martin Tůma <tumic@cbox.cz>
2024-06-15 11:29:06 +0200

    German translation update

--- lang/gpxsee_de.ts
+++ lang/gpxsee_de.ts
@@ -1887,7 +1887,7 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="645"/>
         <source>Lightening:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aufhellung:</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="702"/>
