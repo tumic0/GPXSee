b6d37c3499b7fa403986dfb82c9ee551e3637406
Martin Tůma <tumic@cbox.cz>
2024-06-15 11:27:13 +0200

    Czech translatin update

--- lang/gpxsee_cs.ts
+++ lang/gpxsee_cs.ts
@@ -1889,7 +1889,7 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="645"/>
         <source>Lightening:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zesvětlení:</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="702"/>
