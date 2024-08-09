57a6f29f6abaa81004fab17254f4e6b9109eae3e
Cloud Esp <Frederic.Darboux@inrae.fr>
2024-08-09 10:56:07 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (480 of 480 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/fr/

--- lang/gpxsee_fr.ts
+++ lang/gpxsee_fr.ts
@@ -1564,7 +1564,7 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="250"/>
         <source>POIs</source>
-        <translation>POI</translation>
+        <translation>POIs</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="274"/>
