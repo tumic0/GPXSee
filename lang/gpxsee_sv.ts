06461547e6a67511bd3c4f907a1b54daa318159b
Åke Engelbrektson <eson@svenskasprakfiler.se>
2024-09-04 07:41:42 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (482 of 482 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/sv/

--- lang/gpxsee_sv.ts
+++ lang/gpxsee_sv.ts
@@ -1887,12 +1887,12 @@
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="388"/>
         <source>Detect pauses</source>
-        <translation>Upptäck pauser</translation>
+        <translation>Identifiera pauser</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="539"/>
         <source>Detection:</source>
-        <translation>Detektion:</translation>
+        <translation>Identifiering:</translation>
     </message>
     <message>
         <location filename="../src/GUI/optionsdialog.cpp" line="658"/>
