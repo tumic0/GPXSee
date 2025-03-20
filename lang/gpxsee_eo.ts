bca335d4b2becdd39d3f91e5a2e75defaca5b62d
Nikolay Korotkiy <sikmir@gmail.com>
2025-03-20 15:47:58 +0100

    Translated using Weblate (Esperanto)
    Currently translated at 91.2% (448 of 491 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/eo/

--- lang/gpxsee_eo.ts
+++ lang/gpxsee_eo.ts
@@ -172,7 +172,7 @@
     <message>
         <location filename="../src/data/data.cpp" line="251"/>
         <source>VTK files</source>
-        <translation type="unfinished"></translation>
+        <translation>VTK dosieroj</translation>
     </message>
     <message>
         <location filename="../src/data/data.cpp" line="252"/>
