765a9f696dd151349c5f2c37355c5f1377958a58
Martin Tůma <tumic@cbox.cz>
2025-08-29 07:58:28 +0200

    Czech translation update

--- lang/gpxsee_cs.ts
+++ lang/gpxsee_cs.ts
@@ -640,13 +640,13 @@
         <location filename="../src/GUI/gui.cpp" line="542"/>
         <location filename="../src/GUI/gui.cpp" line="973"/>
         <source>Next graph tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Následující graf</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="547"/>
         <location filename="../src/GUI/gui.cpp" line="975"/>
         <source>Previous graph tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Předchozí graf</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="555"/>
