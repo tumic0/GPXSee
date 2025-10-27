b0ebfd79400b067f5c9738d29c6e1a657df8a828
mtriau <283571717@qq.com>
2025-10-27 04:20:47 +0100

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (501 of 501 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/zh_Hans/

--- lang/gpxsee_zh_CN.ts
+++ lang/gpxsee_zh_CN.ts
@@ -72,17 +72,17 @@
     <message>
         <location filename="../src/map/coros4map.cpp" line="397"/>
         <source>All</source>
-        <translation type="unfinished">所有图层</translation>
+        <translation>所有图层</translation>
     </message>
     <message>
         <location filename="../src/map/coros4map.cpp" line="397"/>
         <source>Landscape</source>
-        <translation type="unfinished"></translation>
+        <translation>地貌图</translation>
     </message>
     <message>
         <location filename="../src/map/coros4map.cpp" line="397"/>
         <source>Topo</source>
-        <translation type="unfinished">等高线图</translation>
+        <translation>等高线图</translation>
     </message>
 </context>
 <context>
