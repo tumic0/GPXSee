c2ada15a045bb04a605e27918ddf497a9353d9a0
Martin Tůma <tumic@cbox.cz>
2026-03-27 23:27:42 +0100

    German translation update

--- lang/gpxsee_de.ts
+++ lang/gpxsee_de.ts
@@ -193,7 +193,7 @@
     <message>
         <location filename="../src/data/data.cpp" line="262"/>
         <source>DJI SRT files</source>
-        <translation type="unfinished"></translation>
+        <translation>DJI SRT-Dateien</translation>
     </message>
     <message>
         <location filename="../src/data/data.cpp" line="263"/>
