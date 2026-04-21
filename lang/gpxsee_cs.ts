1aec19184c2f5f4a148de2c5d283db3e08bc446d
Martin Tůma <tumic@cbox.cz>
2026-04-21 22:53:35 +0200

    Czech translation update

--- lang/gpxsee_cs.ts
+++ lang/gpxsee_cs.ts
@@ -504,7 +504,7 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="243"/>
         <source>Browse directory...</source>
-        <translation type="unfinished"></translation>
+        <translation>Procházet adresář...</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="264"/>
