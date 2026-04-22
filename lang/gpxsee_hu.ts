e111e5b52c6f563e6000d2f67132952df58cc525
99 efi <fegyed71@gmail.com>
2026-04-22 10:40:51 +0200

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (512 of 512 strings)
    
    Translation: GPXSee/Translations
    Translate-URL: https://hosted.weblate.org/projects/gpxsee/translations/hu/

--- lang/gpxsee_hu.ts
+++ lang/gpxsee_hu.ts
@@ -453,7 +453,7 @@
     <message>
         <location filename="../src/GUI/gui.cpp" line="243"/>
         <source>Browse directory...</source>
-        <translation type="unfinished"></translation>
+        <translation>Könyvtár böngészése...</translation>
     </message>
     <message>
         <location filename="../src/GUI/gui.cpp" line="249"/>
