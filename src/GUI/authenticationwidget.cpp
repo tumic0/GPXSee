1b16b90df5c65eec06cc0aefb7263a901ea5b473
Martin Tůma <tumic@cbox.cz>
2026-06-12 19:01:59 +0200

    Last remaining macos style runtime config stuff

--- src/GUI/authenticationwidget.cpp
+++ src/GUI/authenticationwidget.cpp
@@ -1,15 +1,17 @@
 #include <QFormLayout>
+#include "macos.h"
 #include "authenticationwidget.h"
 
 AuthenticationWidget::AuthenticationWidget(QWidget *parent) : QWidget(parent)
 {
 	_username = new QLineEdit();
 	_password = new PasswordEdit();
-#ifdef Q_OS_MAC
-	/* A hack to fix the issue with different field sizes on Mac */
-	_username->setMinimumWidth(150);
-	_password->setMinimumWidth(150);
-#endif // Q_OS_MAC
+
+	if (MacOS::match(style())) {
+		/* A hack to fix the issue with different field sizes */
+		_username->setMinimumWidth(150);
+		_password->setMinimumWidth(150);
+	}
 
 	QFormLayout *layout = new QFormLayout();
 	layout->addRow(tr("Username:"), _username);
--- src/GUI/infolabel.cpp
+++ src/GUI/infolabel.cpp
@@ -1,15 +1,15 @@
-#include <QtGlobal>
+#include "macos.h"
 #include "infolabel.h"
 
 InfoLabel::InfoLabel(const QString &text, QWidget *parent)
   : QLabel(text, parent)
 {
 	QFont f(font());
-#ifdef Q_OS_MAC
-	f.setPointSize(qMax(10, f.pointSize() - 2));
-#else // Q_OS_MAC
-	f.setPointSize(f.pointSize() - 1);
-#endif // Q_OS_MAC
+	if (MacOS::match(style()))
+		f.setPointSize(qMax(10, f.pointSize() - 2));
+	else
+		f.setPointSize(f.pointSize() - 1);
+
 	setWordWrap(true);
 	setFont(f);
 }
