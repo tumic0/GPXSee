d8f40bd4318c12ac50822514590854e80346b298
Martin Tůma <tumic@cbox.cz>
2026-07-02 23:50:44 +0200

    Silenced Qt5 build warning

--- src/GUI/macos.cpp
+++ src/GUI/macos.cpp
@@ -16,6 +16,7 @@ bool MacOS::match(const QStyle *style)
 #if QT_VERSION >= QT_VERSION_CHECK(6, 1, 0)
 	return (style->name().toLower() == "macos");
 #else // QT6
+	Q_UNUSED(style);
 #ifdef Q_OS_MACOS
 	return true;
 #else // Q_OS_MACOS
