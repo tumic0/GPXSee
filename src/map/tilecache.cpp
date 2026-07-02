9fde23e9aec54b985962c22dbd4d8db519ce5f6f
Martin Tůma <tumic@cbox.cz>
2026-07-02 08:44:06 +0200

    Make clear that Qt5 uses int as the cache cost

--- src/map/tilecache.cpp
+++ src/map/tilecache.cpp
@@ -5,9 +5,9 @@ QCache<TileCache::Key, QPixmap> TileCache::_cache;
 
 bool TileCache::insert(const Key &key, QPixmap *pixmap)
 {
-	qsizetype cost = (static_cast<quint64>(pixmap->width())
+	int cost = (static_cast<quint64>(pixmap->width())
 	  * static_cast<quint64>(pixmap->height())
 	  * (static_cast<quint64>(pixmap->depth()) >> 3)) >> 10;
 
-	return _cache.insert(key, pixmap, qMax(cost, 1ll));
+	return _cache.insert(key, pixmap, qMax(cost, 1));
 }
